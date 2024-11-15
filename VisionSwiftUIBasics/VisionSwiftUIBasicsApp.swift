f36d39b8ae10d7245038aef1f13b0bfcd0c078ca
mm-110 <massimomontanaro.1993@gmail.com>
2024-11-15 14:38:15 +0100

    Initial Commit

---
+++ VisionSwiftUIBasics/VisionSwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionSwiftUIBasicsApp.swift
+//  VisionSwiftUIBasics
+//
+//  Created by Massimo Montanaro on 15/11/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionSwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisionSwiftUIBasicsTests/VisionSwiftUIBasicsTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionSwiftUIBasicsTests.swift
+//  VisionSwiftUIBasicsTests
+//
+//  Created by Massimo Montanaro on 15/11/24.
+//
+
+import Testing
+@testable import VisionSwiftUIBasics
+
+struct VisionSwiftUIBasicsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
